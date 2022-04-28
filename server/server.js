c646e2be7e5d364d86f07d55bcb6a1f767a3deb4
kalen510 <kalen.wiley@gmail.com>
2022-04-27 22:37:41 -0400

    initialize apollo server w express

--- server/server.js
+++ server/server.js
@@ -1,10 +1,46 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// only applies to REST API
+// const routes = require('./routes');
+
+// Import Apollo Server
+import { ApolloServer } from 'apollo-server-express';
+// Import authMiddleware
+import { authMiddleware } from require('./utils/auth');
+// import typeDefs and resolvers for GraphQL
+// <-- Add Here -->
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Apply Apollo Server as express middleware
+const startServer = async () => {
+  // create new Apollo Server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context method set to return what want available to the resolvers
+    // ensures every request performs an auth check
+    // updated request object will be passed to resolvers as the context
+    context: authMiddleware
+  });
+
+  // start the apollo server
+  await server.start();
+  
+  // integrate Apollo server with express app as middleware
+  // connect out apollo server to out express server
+  // creates special '/graphql' endpoint for the express server
+  // endpoint will serve as the main endpoint for accessing the entire API
+  server.applyMiddleware({ app });
+
+  // log where we can test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+// intialize the apollo server
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +50,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// only applies to REST API
+// app.use(routes);
+
+// wildcard GET route to respond with production ready front-end code if a request is made to invalid route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,21 +6,25 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
+  // verify JWT in headers
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // allows token to be sent via req.body or req.query or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
+    // separate "Bearer" from "<tokenvalue>"
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
+    // if no token, respond with error message
     if (!token) {
       return res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
     try {
+      // decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
@@ -31,6 +35,8 @@ module.exports = {
     // send to next endpoint
     next();
   },
+
+  // exports a user object and will add the payload properties to the token
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
