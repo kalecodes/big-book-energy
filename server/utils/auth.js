e39c3263619474c5980274517494c19f2f47ad2b
kalen510 <kalen.wiley@gmail.com>
2022-04-30 23:02:44 -0400

    remove code for REST API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,7 +7,7 @@ const expiration = '2h';
 module.exports = {
   // function for our authenticated routes
   // verify JWT in headers
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function (req) {
     // allows token to be sent via req.body or req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
@@ -19,7 +19,7 @@ module.exports = {
 
     // if no token, respond with error message
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -29,11 +29,9 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    return req;
   },
 
   // exports a user object and will add the payload properties to the token
