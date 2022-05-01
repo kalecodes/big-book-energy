52cf04550edc8f4389ba6e181ec2accd5f77c454
kalen510 <kalen.wiley@gmail.com>
2022-05-01 02:07:38 -0400

    +no+

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -10,9 +10,10 @@ import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const userData = data.me;
-
   const [removeBook] = useMutation(REMOVE_BOOK);
+  const userData = data?.me;
+
+  
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
