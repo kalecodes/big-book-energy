9dcec7fc521da1833d2fbe230431805fa9219834
kalen510 <kalen.wiley@gmail.com>
2022-04-30 22:29:57 -0400

    refactor login form:

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,6 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-// API only
-// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 import { LOGIN_USER } from '../utils/mutations';
@@ -14,7 +11,7 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
-  const [login, { error }] = useMutation(LOGIN_USER);
+  const [login] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -32,19 +29,15 @@ const LoginForm = () => {
     }
 
     try {
-      // const response = await loginUser(userFormData);
-      
       const { data } = await login({
-        variables: { userFormData }
+        variables: { ...userFormData }
       });
 
-      const user = data.login.user;
       const token = data.login.token
-      // const { token, user } = await response.json();
-      console.log(user);
       Auth.login(token);
-    } catch (error) {
-      console.error(error);
+
+    } catch (err) {
+      console.error(err);
       setShowAlert(true);
     }
 
